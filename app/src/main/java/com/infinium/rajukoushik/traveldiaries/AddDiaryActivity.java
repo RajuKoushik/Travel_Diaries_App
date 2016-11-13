933cca0e3a3a8671bc77f394dd846ccb7d97ed8b
rajukoushik <g.rajukoushik@gmail.com>
2016-11-13 18:14:23 +0530

    Updated Add Diary - Rest API integration done

--- app/src/main/java/com/infinium/rajukoushik/traveldiaries/AddDiaryActivity.java
+++ app/src/main/java/com/infinium/rajukoushik/traveldiaries/AddDiaryActivity.java
@@ -3,27 +3,108 @@ package com.infinium.rajukoushik.traveldiaries;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddDiaryActivity extends AppCompatActivity {
+
+    public static final String ADDPOST_URL = "http://192.168.43.178:8000/td/post/diary/";
+    String status;
+
+    private EditText editTextDiaryName;
+    private EditText editTextDiaryBrief;
+
+
+    AutoCompleteTextView autoCompleteTextView1;
+
+
+
+
+    public static final String KEY_DIARYBRIEF = "diary_brief";
+    public static final String KEY_DIARYNAME = "diary_name";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_diary);
 
-        AutoCompleteTextView autoCompleteTextView1 = (AutoCompleteTextView) findViewById(R.id.list_of_diaries);
-        autoCompleteTextView1.setAdapter(getEmailAddressAdapter(this));
+
+
+        editTextDiaryName = (EditText) findViewById(R.id.diaryName);
+        editTextDiaryBrief = (EditText) findViewById(R.id.diaryBrief);
     }
 
-    private ArrayAdapter<String> getEmailAddressAdapter(Context context) {
-        DiaryListManager dlm = new DiaryListManager();
-        ArrayList<String> diariesList = dlm.getDiariesList();
 
 
-        return new ArrayAdapter<String>(context, android.R.layout.simple_dropdown_item_1line, diariesList);
+    public void addDIaryBtn(View view) {
+
+        addDiary();
+    }
+
+    private void addDiary() {
+
+        final String diary_name = editTextDiaryName.getText().toString().trim();
+        final String diary_brief= editTextDiaryBrief.getText().toString().trim();
+
+
+
+
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, ADDPOST_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jobj = new JSONObject(response);
+                            status = jobj.getString("status");
+
+                            Toast.makeText(AddDiaryActivity.this,status.toString(), Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(AddDiaryActivity.this,error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put(KEY_DIARYNAME,diary_name);
+                params.put(KEY_DIARYBRIEF,diary_brief);
+
+
+
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
