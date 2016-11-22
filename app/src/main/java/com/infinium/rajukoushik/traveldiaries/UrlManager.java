b508cb9a9c22b5c03e9c5468dab1dae731e77ffb
rajukoushik <g.rajukoushik@gmail.com>
2016-11-22 21:03:34 +0530

    Included A Url Manager.

---
+++ app/src/main/java/com/infinium/rajukoushik/traveldiaries/UrlManager.java
@@ -0,0 +1,13 @@
+package com.infinium.rajukoushik.traveldiaries;
+
+/**
+ * Created by rajukoushik on 22/11/16.
+ */
+public class UrlManager {
+    static String url = "http://192.168.43.178:8000/";
+
+
+    public String getUrl() {
+        return url;
+    }
+}
